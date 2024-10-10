ae1f1f58b59d7dc4e53e93662a33655d7b6e8ef2
flopesde <filipe200240@gmail.com>
2024-10-10 17:38:13 +0200

    Add db_conf.js to .gitignore

--- models/db_conf.js
+++
@@ -1,6 +0,0 @@
-// file db_conf.js in models
-
-const db = require('better-sqlite3')('db/exoplanets.db', { verbose: console.log });
-// const db = require('better-sqlite3')('/home/olivier/exoplanets.db', { verbose: console.log });
-
-module.exports = db;
